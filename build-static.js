d145f153937be5ccea34684f7956c8d01a38b779
David <david@Dmitris-Mac-mini.local>
2026-04-28 21:23:11 +0300

    Add Vercel static deployment

---
+++ build-static.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+const output = path.join(__dirname, "dist");
+fs.rmSync(output, { force: true, recursive: true });
+fs.mkdirSync(output, { recursive: true });
+
+for (const file of ["index.html", "styles.css", "game.js", "vercel.json"]) {
+  fs.copyFileSync(path.join(__dirname, file), path.join(output, file));
+}
